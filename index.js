f4e0320d99bf4badef3986975e96b820a70e3449
herloncosta <herlon36@gmail.com>
2022-05-27 22:23:59 -0300

    config routes

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import express from "express";
+import clientsRouter from "./routes/client.route.js";
+import productsRouter from "./routes/product.route.js";
+import suppliersRouter from "./routes/supplier.route.js";
+import salesRouter from "./routes/sale.route.js";
 
 const app = express();
+app.use(express.json());
+
+app.use("/client", clientsRouter);
+app.use("/product", productsRouter);
+app.use("/supplier", suppliersRouter);
+app.use("/sale", salesRouter);
 
 app.listen(3000, () => console.log("API Started!"));
