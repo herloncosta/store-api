e3f178092d4343048107e083704566486eb1e701
herloncosta <herlon36@gmail.com>
2022-05-27 22:41:25 -0300

    config && export winston with global.logger

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 import express from "express";
+import cors from "cors";
+import winston, { Logform } from "winston";
 import clientsRouter from "./routes/client.route.js";
 import productsRouter from "./routes/product.route.js";
 import suppliersRouter from "./routes/supplier.route.js";
 import salesRouter from "./routes/sale.route.js";
 
+const { combine, timestamp, label, printf } = winston.format;
+
+const logFormat = printf(({ level, message, timestamp, label }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+global.logger = winston.createLogger({
+    level: "silly",
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: "store-api.log" }),
+    ],
+    format: combine(label({ label: "store-api" }), timestamp(), Logform),
+});
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/client", clientsRouter);
 app.use("/product", productsRouter);
