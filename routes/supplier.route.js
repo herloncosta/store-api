64c23dfee33a663dbe3a1a800d3a3678552e2ff9
herloncosta <herlon36@gmail.com>
2022-06-07 20:15:03 -0300

    supplier route

--- routes/supplier.route.js
+++ routes/supplier.route.js
@@ -1,5 +1,12 @@
 import express from "express";
+import SupplierController from "../controllers/supplier.controller.js";
 
 const router = express.Router();
+
+router.post("/", SupplierController.createSupplier);
+router.get("/", SupplierController.getSuppliers);
+router.get("/:id", SupplierController.getSupplier);
+router.put("/:id", SupplierController.updateSupplier);
+router.delete("/:id", SupplierController.deleteSupplier);
 
 export default router;
